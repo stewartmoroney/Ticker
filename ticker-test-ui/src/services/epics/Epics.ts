2f1316eea5788df081e9dbfba6649899d6e30c0d
Stewart Moroney <stewartmoroney@googlemail.com>
2018-02-15 23:15:59 +0000

    redux dev tools

--- ticker-test-ui/src/services/epics/Epics.ts
+++ ticker-test-ui/src/services/epics/Epics.ts
@@ -1,30 +1,25 @@
 import { ActionsObservable, combineEpics } from 'redux-observable';
+import { Action } from 'redux';
 import 'rxjs';
 
 import Services from '../../services/Services';
 import { SUBSCRIBE, UNSUBSCRIBE } from '../redux/ActionTypes';
 
-export const loggingEpic: any = (action$: ActionsObservable<any>) =>
-  action$.filter((action: any) => true)
-    .do((item: any) => console.log(item))
-    .ignoreElements();
-
-export const susbscribeEpic: any = (action$: ActionsObservable<any>) =>
-  action$.filter((action: any) => action.type === SUBSCRIBE)
+export const susbscribeEpic: any = (action$: ActionsObservable<Action>) =>
+  action$.ofType(SUBSCRIBE)
     .do((action: any) => {
       Services.subscribeService().subscribe(action.payload);
     })
     .ignoreElements();
 
 export const unsusbscribeEpic: any = (action$: ActionsObservable<any>) =>
-  action$.filter((action: any) => action.type === UNSUBSCRIBE)
+  action$.ofType(UNSUBSCRIBE)
     .do((action: any) => {
       Services.subscribeService().unsubscribe(action.payload);
     })
     .ignoreElements();
 
 export const rootEpic = combineEpics(
-  loggingEpic,
   susbscribeEpic,
   unsusbscribeEpic
 );
--- ticker-test-ui/src/services/redux/Store.ts
+++ ticker-test-ui/src/services/redux/Store.ts
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './Reducer';
 import { rootEpic } from '../epics/Epics';
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
 
-export default createStore(rootReducer, applyMiddleware(epicMiddleware));
+export default createStore(rootReducer, 
+		composeWithDevTools(
+			applyMiddleware(epicMiddleware)
+		)
+	,);
