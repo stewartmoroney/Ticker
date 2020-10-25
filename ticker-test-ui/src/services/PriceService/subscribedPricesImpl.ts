8bb638ce02520c162c51003760254a761ece623e
StewartMoroney <stewartmoroney@googlemail.com>
2020-10-25 20:48:26 +0000

    Price reducer fix

--- ticker-test-ui/src/services/PriceService/subscribedPricesImpl.ts
+++ ticker-test-ui/src/services/PriceService/subscribedPricesImpl.ts
@@ -13,15 +13,21 @@ type PriceMessage = {
 const isPriceMessage = (data: Message): data is PriceMessage =>
   data.type === "PriceUpdate";
 
+type PriceState = Price[];
+
+const priceReducer = (acc: PriceState, p: Price) => {
+  const index = acc.findIndex(ap => ap.instrumentId === p.instrumentId);
+  return [...acc.slice(0, index), p, ...acc.slice(index, acc.length)];
+};
+
 export const subscribedPrices$ = defer(() =>
   getMessages$().pipe(
     filter(isPriceMessage),
     map(msg => msg.price),
-    scan((acc, p) => [...acc, p], [] as Price[]),
-    shareLatest()
+    scan(priceReducer, [])
   )
 );
 
 export const [useSubscribedPrices] = bind(
-  subscribedPrices$.pipe(startWith([] as Price[]))
+  subscribedPrices$.pipe(startWith([] as PriceState))
 );
