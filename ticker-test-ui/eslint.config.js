f45c3cfe3525d5a1a1d3d44f20acaa0a0c206cca
StewartMoroney <stewartmoroney@googlemail.com>
2025-04-01 20:15:44 +0100

    eslint updates

---
+++ ticker-test-ui/eslint.config.js
@@ -0,0 +1,17 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    extends: [eslint.configs.recommended, tseslint.configs.strict],
+    files: ["src/**/*.js", "src/**/*.ts", "src/**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+]);
